471c03fc303046b2dbe745d3d6dee5d148a14147
Anurag <asnksharma6@gmail.com>
2023-10-11 21:07:03 +0530

    sum of two numbers

---
+++ secondFile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+
+    cout << "enter value of a :" << endl;
+    cin >> a;
+    cout << "enter value of b :" << endl;
+    cin >> b;
+    cout << "Sum of a and b is :" << endl;
+    cout << a + b << endl;
+
+    return 0;
+}
